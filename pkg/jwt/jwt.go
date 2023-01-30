d35a2357af5460cecd971d39246418070c176c5e
handsomeyzz <1638393647@qq.com>
2023-01-30 23:07:32 +0800

    Initial commit

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -9,7 +9,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-var mySecret = []byte("夏天夏天悄悄过去")
+var mySecret = []byte("hellonil")
 
 // MyClaims 自定义声明结构体并内嵌jwt.StandardClaims
 // jwt包自带的jwt.StandardClaims只包含了官方字段
@@ -30,7 +30,7 @@ func GenToken(userID int64, username string) (string, error) {
 		jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(
 				time.Duration(viper.GetInt("auth.jwt_expire")) * time.Hour).Unix(), // 过期时间
-			Issuer: "bluebell", // 签发人
+			Issuer: "hellonil", // 签发人
 		},
 	}
 	// 使用指定的签名方法创建签名对象
